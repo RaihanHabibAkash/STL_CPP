0cc6aac2ab251008eafce1f2febf6df39805e70a
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-25 16:57:16 +0600

    Range base loop added

--- Tuple/vector.cpp
+++ Tuple/vector.cpp
@@ -13,5 +13,7 @@ int main() {
     for(int i = 0; i < 3; i++)
         cout << get<0>(student[i]) << " " << get<1>(student[i]) << " " << get<2>(student[i]) << endl;
     
+    for(auto [x, y, z] : student)
+        cout << x << " " << y << " " << z << endl;
     return 0;
 }
