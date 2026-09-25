00981dd74e5878711af2d1033fd7d33214ab8de4
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-25 13:55:09 +0600

    insertion

---
+++ Tuple/insertion.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {    
+    // tuple<string, int, int, char> student = make_tuple("Akash", 10, 3, 'A');
+    tuple<string, int, int, char> student = {"Akash", 10, 3, 'A'};
+    
+    return 0;
+}
